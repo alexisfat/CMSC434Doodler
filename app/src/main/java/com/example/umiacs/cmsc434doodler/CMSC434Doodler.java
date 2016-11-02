b6935abc22b23dc016d62d8cf91ed4e9f2c339bc
Alex Berlinger <alexberlinger@aol.com>
2016-11-02 18:11:03 -0400

    View

---
+++ app/src/main/java/com/example/umiacs/cmsc434doodler/CMSC434Doodler.java
@@ -0,0 +1,13 @@
+package com.example.umiacs.cmsc434doodler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CMSC434Doodler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cmsc434_doodler);
+    }
+}
