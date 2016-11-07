40a91dd7e0845976275c323aa13100bdb64c6fb5
Alex Berlinger <alexberlinger@aol.com>
2016-11-07 13:07:30 -0500

    finished prototype doodler projet

--- app/src/main/java/com/example/umiacs/cmsc434doodler/CMSC434Doodler.java
+++ app/src/main/java/com/example/umiacs/cmsc434doodler/CMSC434Doodler.java
@@ -1,13 +1,215 @@
 package com.example.umiacs.cmsc434doodler;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CMSC434Doodler extends AppCompatActivity {
+public class CMSC434Doodler extends AppCompatActivity  implements CompoundButton.OnCheckedChangeListener {
+    Switch erase;
+    private int rProgress, bProgress, gProgress, opacity;
+    private float brushSize;
+    private DoodleView doodle;
+    private SeekBar redSeekBar, blueSeekBar, greenSeekBar, opacitySeekBar, brushSizeSeekBar;
+    private Button clear, undo, save;
+  //  private Switch erase;
+
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cmsc434_doodler);
+    protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_cmsc434_doodler);
+
+            doodle = (DoodleView) findViewById(R.id.doodleCanvas);
+            redSeekBar = (SeekBar) findViewById(R.id.colorSliderRed);
+            blueSeekBar = (SeekBar) findViewById(R.id.colorSliderBlue);
+            greenSeekBar = (SeekBar) findViewById(R.id.colorSliderGreen);
+            opacitySeekBar = (SeekBar) findViewById(R.id.opacitySeekBar);
+            brushSizeSeekBar = (SeekBar) findViewById(R.id.brushSizeSeekBar);
+            undo = (Button) findViewById(R.id.undoButton);
+            clear = (Button) findViewById(R.id.clearButton);
+            // redo = (Button) findViewById(R.id.redoButton);
+            // erase = (Button) findViewById(R.id.eraserButton);
+            save = (Button) findViewById(R.id.saveButton);
+            erase = (Switch) findViewById(R.id.eraserSwitch);
+            erase.setOnCheckedChangeListener(this);
+            redSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                    rProgress = progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                /*Context context = getApplicationContext();
+                CharSequence text = "Your progress is " + seekBar.getProgress();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();*/
+                }
+            });
+
+            blueSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                    bProgress = progress;
+                    changeColor();
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                /*Context context = getApplicationContext();
+                CharSequence text = "Your progress is " + seekBar.getProgress();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();*/
+                }
+            });
+
+            greenSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                    gProgress = progress;
+                    changeColor();
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+              /*  Context context = getApplicationContext();
+                CharSequence text = "Your progress is " + seekBar.getProgress();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();*/
+                }
+            });
+
+            opacitySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    opacity = i;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    doodle.changeOpacity(opacity);
+                }
+
+            });
+
+            brushSizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    brushSize = i;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    doodle.changeBrushSize((float) brushSize);
+                }
+
+            });
+
+
+            undo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    doodle.undo();
+                }
+            });
+
+            clear.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    doodle.clear();
+                }
+            });
+
+         /* redo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    doodle.redo();
+                }
+            });*/
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Saved to gallery!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                }
+            });
+    }
+
+
+    private void changeColor() {
+        int color = Color.rgb(rProgress, gProgress, bProgress);
+        doodle.changeColor(color);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if(isChecked) {
+            //   erase.setText("ON");  //To change the text near to switch
+            DoodleView.erase = true;
+            doodle.draw();
+            //}
+        } else {
+            //erase.setText("OFF");   //To change the text near to switch
+            DoodleView.erase = false;
+            doodle.draw();
+        }
     }
 }
+
+
+
+
--- app/src/main/java/com/example/umiacs/cmsc434doodler/DoodleView.java
+++ app/src/main/java/com/example/umiacs/cmsc434doodler/DoodleView.java
@@ -9,6 +9,10 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+
+import java.util.ArrayList;
+
+
 /**
  * Created by umiacs on 11/2/16.
  */
@@ -16,6 +20,20 @@ import android.view.View;
 class DoodleView extends View {
     private Paint _paintDoodle = new Paint();
     private Path _path = new Path();
+    private ArrayList<Path> _paths;
+    private ArrayList<Paint> _paintDoodles;
+   // private ArrayList<Path> _allPaths;
+   // private ArrayList<Paint> _allPaints;
+    private int _opacity;
+    private int _color;
+    private float _brushSize;
+    public static boolean erase;
+
+  //  private int undoCount=0;
+  //  private List<Paint> undoPaints = new ArrayList<>();
+   // private List<Path> undoPaths = new ArrayList<>();
+
+
 
     public DoodleView(Context context) {
         super(context);
@@ -33,18 +51,64 @@ class DoodleView extends View {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
-        _paintDoodle.setColor(Color.RED);
+        _color = Color.BLACK;
+        _opacity = 100;
+        _brushSize = 2;
+        erase = false;
+        _path = new Path();
+        _paintDoodle = new Paint();
+        _paintDoodle.setStyle(Paint.Style.STROKE);
+
+        _paths = new ArrayList<>();
+        _paintDoodles = new ArrayList<>();
+        _paintDoodle.setColor(_color);
         _paintDoodle.setAntiAlias(true);
         _paintDoodle.setStyle(Paint.Style.STROKE);
+        _paintDoodle.setStrokeJoin(Paint.Join.ROUND);
+        _paintDoodle.setStrokeWidth(_brushSize);
+        _paintDoodle.setAlpha(_opacity);
     }
+
+    public void draw() {
+        if(!erase) {
+            _paintDoodle = new Paint();
+            _path = new Path();
+            _paintDoodle.setAntiAlias(true);
+            _paintDoodle.setStyle(Paint.Style.STROKE);
+            _paintDoodle.setColor(_color);
+            _paintDoodle.setStrokeWidth(_brushSize);
+            _paintDoodle.setAlpha(_opacity);
+        } else {
+            _paintDoodle = new Paint();
+            _path = new Path();
+            _paintDoodle.setAntiAlias(true);
+            _paintDoodle.setStyle(Paint.Style.STROKE);
+            _paintDoodle.setARGB(255, 249, 249, 249);
+           // _paintDoodle.setColor(Color.rgb(249,249,249));
+            _paintDoodle.setStrokeWidth(100);
+           // _paintDoodle.setAlpha(255);
+        }
+        //invalidate();
+    }
+
+
+
+
 
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         // canvas.drawLine(0,0, getWidth(), getHeight(), _paintDoodle);
+      //  _paintDoodle.setStyle(Paint.Style.STROKE);
+        //System.out.println("OnDraw Method " +_paintDoodle.getStyle());
         canvas.drawPath(_path, _paintDoodle);
+
+        for(int i=0; i<_paths.size(); i++){
+            canvas.drawPath(_paths.get(i), _paintDoodles.get(i));
+        }
+        invalidate();
     }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
@@ -53,15 +117,72 @@ class DoodleView extends View {
 
         switch (motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                //_path.addCircle(touchX, touchY, (float)_brushSize/16, Path.Direction.CCW );
                 _path.moveTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
                 _path.lineTo(touchX, touchY);
+
                 break;
             case MotionEvent.ACTION_UP:
+                _paths.add(_path);
+                _paintDoodles.add(_paintDoodle);
+                _path = new Path();
+                _paintDoodle = new Paint();
+                _paintDoodle.setStyle(Paint.Style.STROKE);
                 break;
         }
+        System.out.println(_paintDoodle.getStyle());
         invalidate();
         return true;
     }
+
+    public void changeOpacity(int op){
+        _opacity = Math.abs(255-op);
+        _paintDoodle.setStyle(Paint.Style.STROKE);
+        draw();
+
+    }
+
+    public void changeBrushSize(float brush){
+        _brushSize = brush;
+        _paintDoodle.setStyle(Paint.Style.STROKE);
+        draw();
+    }
+
+    public void changeColor(int color){
+        _color = color;
+        _paintDoodle.setStyle(Paint.Style.STROKE);
+        draw();
+    }
+
+    public void undo(){
+        if(_paths.size() > 0 && _paintDoodles.size() > 0){
+            _paths.remove(_paths.size() - 1);
+            _paintDoodles.remove(_paintDoodles.size() - 1);
+            invalidate();
+        }
+    }
+
+    /*public void redo(){
+        //_path.rewind();
+        /*if(_allPaints.size() > 0 && _allPaths.size() > 0) {
+            _paintDoodles.add(_allPaints.get(_allPaints.size() - undoCount + 1));
+            _paths.add(_allPaths.get(_allPaths.size() - undoCount + 1));
+        }
+        if(!undoPaths.isEmpty() && !undoPaints.isEmpty()){
+
+            _paths.add(undoPaths.get(undoCount));
+            _paintDoodles.add(undoPaints.get(undoCount));
+        }
+        invalidate();
+    }*/
+
+
+    public void clear(){
+        _paths.clear();
+        _paintDoodles.clear();
+        invalidate();
+    }
+
 }
